b100a1b76cfbca504909508ad3fd7cb544b499dc
ArtByArti <sarsavbuk@gmail.com>
2021-03-28 18:19:05 +0700

    Create Map file

---
+++ Map.h
@@ -0,0 +1,30 @@
+#include <SFML\Graphics.hpp>
+const int HEIGHT_MAP = 20;
+const int WIDTH_MAP = 20;
+
+sf::String TileMap[HEIGHT_MAP] = (
+	"00000000000000000000"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"0                  0"
+	"00000000000000000000"
+
+
+
+
+)
